68363ee370af9ed8cf1e49020994e03ec054a5fc
Ngan Phan (300180184) <kimngan.9at2@gmail.com>
2024-02-07 17:26:29 -0800

    Virtual assistant powered by GPT model 3.5

--- VA.py
+++ VA.py
@@ -1,17 +1,18 @@
 import speech_recognition as sr
 import pyttsx3
 from datetime import date, datetime
+from openai import OpenAI
 
+API_key = open("OpenAI_key.txt", "r").readline()
 # initialize
 robot_ear = sr.Recognizer()
 robot_mouth = pyttsx3.init()
-robot_brain = ""
+openAI = OpenAI(api_key=API_key)
 
 while True:
     # listen: speech to text
     with sr.Microphone() as mic:
         print("Robot: I'm listening")
-        # robot_mouth.say("Hi, what is your name?")
         audio = robot_ear.listen(mic)
 
     print("Robot: ...")
@@ -30,7 +31,7 @@ while True:
     if you == "":
         robot_brain = "I can't hear you, try again"
     elif "hello" in you:
-        robot_brain = "Hello Ngan"
+        robot_brain = "Hello Ngan, how can I help you today?"
     elif "today" in you: 
         today = date.today()
         robot_brain = today.strftime("%B %d, %Y")
@@ -43,7 +44,16 @@ while True:
         robot_mouth.runAndWait()
         break
     else:
-        robot_brain = "Sorry I don't understand"
+        response = openAI.chat.completions.create(
+        messages=[
+            {
+                "role": "user",
+                "content": you,
+            }
+        ],
+        model="gpt-3.5-turbo",
+    )
+        robot_brain = response.choices[0].message.content
 
     print("Robot: " + robot_brain)
         
